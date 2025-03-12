d0ceefa09bee11facc7ad85c81a151a63bec8837
Emanuelprime <emanuelll3000@gmail.com>
2025-03-12 20:30:48 -0300

    Atividade04 Inicio

---
+++ First-Period/1bim/PHP/Atividade04/index.php
@@ -0,0 +1,36 @@
+<?php 
+class Produtos{
+	private $nome;
+	private $preco;
+	private $quantidade;
+}
+
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sistema de Gestão de Produtos</title>
+</head>
+<body>
+	<h1>Sistema de Gestão de Produtos</h1>
+	<form method="post" action="">
+	<a href="./alteraEstoque.php">
+		<button type="button">Alterar Estoque</button>
+	</a>
+	<a href="./alteraPreco.php">
+		<button type="button">Alterar Preço</button>
+	</a>
+
+	</form>
+
+</body>
+</html>
