315e3fd7f56e2cd838974efeefe56f5fd534a751
Emanuelprime <emanuelll3000@gmail.com>
2025-03-13 08:59:10 -0300

    Bug Fix

--- First-Period/1bim/PHP/Atividade03/index.php
+++ First-Period/1bim/PHP/Atividade03/index.php
@@ -1,90 +1,73 @@
 <?php
-$status = "";
-$notas = "";
-$mostrarNotas = false;
-$numeros =[];
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nomeAluno = $_POST['nomeAluno'] ?? '';
-    $notaAluno = $_POST['notaAluno'] ?? '';
-    $mostrarNotas = isset($_POST['mostrarNotas']);
-    $limparNotas = isset($_POST['limparNotas']);
-    //adiciona nota do aluno
-    if (!empty($nomeAluno) && !empty($notaAluno) && !$mostrarNotas) {
-        $aluno = '<strong>Nome:</strong> ' . $nomeAluno . '<br><strong>Nota:</strong> ' . $notaAluno . '<br>';
-        $file = fopen("notas.txt", "a+");
-        if ($file) {
-            $verificaAluno = "";
-            if(filesize("notas.txt") > 0){
-            $verificaAluno = fread($file, filesize("notas.txt"));} // Lê o arquivo
-            fclose($file);
+session_start();
+class Aluno
+{
+    private $nome;
+    private $matricula;
+    private $curso;
 
-            //verificação para ver se o nomeAluno está no verificaAluno, se não estiver (=false) ele adiciona o aluno 
-            if (strpos($verificaAluno, $nomeAluno) === false) {
-                $file = fopen("notas.txt", "a");
-                fwrite($file, $aluno . "\n");
-                fclose($file);
-                $status = "Nota cadastrada com sucesso!";
-            } else {
-                $status = "Aluno já cadastrado!";
-            }
-        } else {
-            $status = "Erro ao abrir o Arquivo!";
-        }
+    public function __construct($nome, $matricula, $curso)
+    {
+        $this->nome = $nome;
+        $this->matricula = $matricula;
+        $this->curso = $curso;
     }
 
-        //pega todos os numeros do arquivo notas.txt
-    if(file_exists("notas.txt")){
-        $file = fopen("notas.txt", "r");
-        if($file){
-            while (($linha = fgets($file)) !== false){
-                preg_match_all('/\d+/', $linha, $matches);
-                
-                //se o matches não estiver vazio, ele adiciona o primeiro numero do array matches[0] no array numeros
-                if(!empty($matches[0])){
-                    $numeros[] = $matches[0][0];
-                }
-            }
-            fclose($file);
-        }
-        
+    public function getNome()
+    {
+        return $this->nome;
     }
-    function mediaAlunos($numeros){ //função para calcular a média dos alunos
-        if(empty($numeros)){
-            return "Não foi possivel calcular média, pois não há notas cadastradas";
-        }
-        $soma = array_sum($numeros);
-        $media = $soma / count($numeros);
-        return number_format($media, 2);
-    } //chama a função mediaAlunos e formata o resultado para 2 casas decimais
-    $media =  mediaAlunos($numeros);
+    public function getCurso()
+    {
+        return $this->curso;
+    }
+    public function getMatricula()
+    {
+        return $this->matricula;
+    }
+}
 
-    if ($mostrarNotas) { //mostra as notas cadastradas
-        $file = fopen("notas.txt", "r+");
-        if ($file) {
-            if (filesize("notas.txt") > 0) {
-                $notas = fread($file, filesize("notas.txt"));
-            }
-            fclose($file);
-        } else {
-            $status = "Erro ao abrir o arquivo!";
-        }
+class CadastroAlunos
+{
+    private $alunos = [];
+
+    public function cadastrarAluno(Aluno $aluno)
+    {
+        $this->alunos[] = $aluno;
     }
 
-    if ($limparNotas) { //limpa as notas cadastradas escrevendo um arquivo vazio
-        $file = fopen("notas.txt", "w");
-        if ($file) {
-            fwrite($file, "");
-            fclose($file);
-            $status = "Notas apagadas com sucesso!";
-            $notas = "";
-        } else {
-            $status = "Erro ao abrir o arquivo!";
-        }
+    public function getAlunos()
+    {
+        return $this->alunos;
     }
+
+   
+
 
 }
+if(!isset($_SESSION['cadastro'])){
+    $_SESSION['cadastro'] = new CadastroAlunos();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = $_POST['nome'];
+    $matricula = $_POST['matricula'];
+    $curso = $_POST['curso'];
+    $aluno = new Aluno($nome, $matricula, $curso);
+    $_SESSION['cadastro']->cadastrarAluno($aluno);
+    header("location: " .  $_SERVER['PHP_SELF']);
+    exit();
+
+}
+
+
+$alunosCadastrados = $_SESSION['cadastro']->getAlunos();
+
+
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -92,39 +75,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Armazenamento de Notas</title>
+    <title>Atividade03</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>Sistema de Armazenamento de Notas</h1>
-    <form method="post" action="">
-        <label for="nomeAluno">Insira o Nome Completo do Aluno</label>
-        <input type="text" name="nomeAluno" required>
-        <br>
-        <label for="notaAluno">Insira a nota do Aluno</label>
-        <input type="number" name="notaAluno" min="0" max="10" required>
-        <br>
-        <input type="submit" value="Enviar">
-        <p><?php echo $status; ?></p>
+    <h1>Atividade Aula 05 Mão na Massa</h1>
+    <form method="POST" action="">
+        <h2>Sistema de Cadastro de Alunos em PHP</h2>
+
+        <input type="text" name="nome" placeholder="Nome" required>
+        <input type="text" name="matricula" placeholder="Matricula" required>
+        <input type="text" name="curso" placeholder="Curso" required>
+        <button type="submit">Cadastrar</button>
+
     </form>
-    <div name="notas">
-        <h2>Notas Cadastradas</h2>
-        <form method="post" action="" class="notas">
-            <div class="botoes">
-                <input type="submit" name="limparNotas" value="Limpar Notas">
-                <input type="submit" name="mostrarNotas" value="Mostrar Notas">
-            </div>
 
-        </form>
+    <h2>Alunos Cadastrados</h2>
+    <table>
+        <tr>
+            <th>Nome</th>
+            <th>Matricula</th>
+            <th>Curso</th>
+        </tr>
         <?php
-        echo nl2br($notas);
+        foreach ($alunosCadastrados as $aluno) {
+            echo "<tr>";
+            echo "<td>" . $aluno->getNome() . "</td>";
+            echo "<td>" . $aluno->getMatricula() . "</td>";
+            echo "<td>" . $aluno->getCurso() . "</td>";
+            echo "</tr>";
+        }
         ?>
-        <h3>Média dos Alunos</h3>
+    </table>
 
-        <p><?php echo $media; ?></p>
-
-    </div>
 </body>
 
 </html>
